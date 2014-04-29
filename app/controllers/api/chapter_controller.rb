4cfa7b483d427ad200ec3515ad1db73ffd06de4f
rob sullivan <rob@datachomp.com>
2014-04-29 10:02:35 -0500

    rough chapter section

--- app/controllers/api/chapter_controller.rb
+++ app/controllers/api/chapter_controller.rb
@@ -2,12 +2,12 @@ class Api::ChapterController < ApplicationController
 
   def list
     @chapters = Chapter.all
-    p @chapters
     render "api/chapter/list", :formats => [:json], :handlers => [:jbuilder], status: 200
   end
 
   def show
-    @chapter = Chapter.find(1)
+    #  should this be by id or by chapter name? What is the flow here
+    @chapter = Chapter.find(params[:id])
     render "api/chapter/show", :formats => [:json], :handlers => [:jbuilder], status: 200
   end
 
---
+++ app/views/api/chapter/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.(@chapter, :id, :title)
