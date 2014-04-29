49701be9d296899a6b1468e41f02bf9d4581cfa9
rob sullivan <rob@datachomp.com>
2014-04-28 22:45:01 -0500

    clean up garbage

--- app/controllers/api/topic_controller.rb
+++ app/controllers/api/topic_controller.rb
@@ -2,11 +2,9 @@ class Api::TopicController < ApplicationController
 
   def show
     @topic = Hash.new
-    @topic["id"] = 1
     @topic["topic"] = 'blahs'
     @topic["beer"] = 'drinks'
-    p @topic
-    #render :json => @topic.to_json, status: 200
+
     render "api/topic/show", :formats => [:json], :handlers => [:jbuilder], status: 200
   end
 
