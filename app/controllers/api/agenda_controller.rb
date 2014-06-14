cf3657864c56fa3b51928f4b645a1ebd76062411
rob sullivan <rob@datachomp.com>
2014-06-14 18:38:58 -0500

    smarter default on api call

--- app/controllers/api/agenda_controller.rb
+++ app/controllers/api/agenda_controller.rb
@@ -1,7 +1,7 @@
 class Api::AgendaController < ApplicationController
 
   def show
-    meeting_date = params[:date].presence || Date.today
+    meeting_date = params[:date].presence || Date.today.at_beginning_of_month
 
     @rawr = Agenda.get_agenda(meeting_date)
     render "api/agenda/show", :formats => [:json], :handlers => [:jbuilder], status: 200
