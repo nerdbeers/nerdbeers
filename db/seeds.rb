4824573105f0940bd6389bd45acca91506829eb6
rob sullivan <rob@datachomp.com>
2014-06-14 11:14:16 -0500

    2014 data migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,12 +4,17 @@ venue = Venue.create({ venue: 'James E. McNellies Public House' , map_link: 'htt
 venue = Venue.create({ venue: 'TapWerks Ale House', map_link: 'https://www.google.com/maps/place/TapWerks+Ale+House+%26+Cafe/@35.466577,-97.5096,17z/data=!3m1!4b1!4m2!3m1!1s0x0:0x8162386058d68e8a?hl=en'})
 venue = Venue.create({ venue: 'Redpin Bowling Lounge', map_link: 'https://www.google.com/maps/place/RedPin+Bowling+Lounge/@35.463632,-97.509842,17z/data=!3m1!4b1!4m2!3m1!1s0x87b2172770ffab75:0x68464e5fb0c7350e?hl=en'})
 
-
+#2013
 agenda = Agenda.create("2013-07-01", 1, "Project auto scaffolding", "Best Chat Client/Protocol", "Microsoft Ending Technet", "Full Sail Session Black", "Black Mesa Blonde", "Rogue Dead Guy", "Thu, July 18, 2013, 5:30 PM")
 agenda = Agenda.create("2013-08-01", 2, "Yeoman", "Mobile App vs Mobile Web", "Favorite Dev Setup (Hardware/Software)", "Green Flash Hop Head Red", "Left Hand Sawtooth", "Zatec Dark", "Thu, August 22, 2013, 5:30 PM")
 agenda = Agenda.create("2013-09-01", 2, "Postgres 9.3", "Plastic iPhones", "Project Visualization", "Carlsberg Pilsner", "Black Sheep- Monty Python Holy Grail", "North Coast - Prankster", "Thu, September 19, 2013, 5:30 PM")
-
-
 agenda = Agenda.create("2013-10-01", 2, "Ubuntu 13.10 * Windows 8.1", "Proliferation of Cordova", "Application Security", "North Coast - Red Seal Ale", "Ommegang -Three Philosophers", "Santa Fe - Happy Camper IPA", "Thu, October 18, 2013, 5:30 PM")
 agenda = Agenda.create("2013-11-01", 1, "Go-lang", "RabbitMq", "Thunder Plains", "Green Flash Hop Head Red", "Shiner Wild Hare", "Coop Gransport Porter", "Thu, November 21, 2013, 5:30 PM")
 agenda = Agenda.create("2013-12-01", 2, "Cloud IDEs", "Arduino Projects", "D3 Tricks", "Big Sky - Trout Slayer", "Black Mesa - Blonde", "Anchor Steam", "Thu, December 19, 2013, 5:30 PM")
+
+#2014
+agenda = Agenda.create("2014-01-01", 3, "", "", "", "", "", "", "Thu, January 16, 2013, 5:30 PM")
+agenda = Agenda.create("2014-02-01", 2, "Docker", "Growth of Coffeescript", "What makes a good API", "Narwhal", "Java Stout", "Avery ipa", "Thu, Feb 20, 2013, 5:30 PM")
+agenda = Agenda.create("2014-03-01", 4, "ElasticSearch", "Side Projects", "Atom", "Coop - F5", "Coop - Native Amber", "Bridgeport IPA", "Thu, March 19, 2013, 5:30 PM")
+agenda = Agenda.create("2014-04-01", 4, "Microsoft/Nasa OSS", "Heartbleed", "Ubuntu 14.04/Windows XP", "Coop - F5", "GreenFlash Hop Head Red", "Coop - Native Amber", "Thu, April 17, 2013, 5:30 PM")
+agenda = Agenda.create("2014-05-01", 4, "canary.io", "Death of TDD", "KcDc", "Avery - Ellies Brown Ale", "Avery - Out of Bounds Stout", "Coop F5", "Thu, May 22, 2013, 5:30 PM")
