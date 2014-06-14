c102b26d6e1d3a417996264cc614b90516842d22
rob sullivan <rob@datachomp.com>
2014-06-14 10:59:28 -0500

    seed agenda entries for 2013

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
 
 venue = Venue.create({ venue: 'Republic Gastropub on Classen Curve', map_link: 'http://goo.gl/maps/1ktTV'})
 venue = Venue.create({ venue: 'James E. McNellies Public House' , map_link: 'https://www.google.com/maps/place/James+E.+McNellie''s+Public+House/@35.479277,-97.521327,17z/data=!3m1!4b1!4m2!3m1!1s0x87b217347cf573e5:0xfc8b711954da4e4'})
 venue = Venue.create({ venue: 'TapWerks Ale House', map_link: 'https://www.google.com/maps/place/TapWerks+Ale+House+%26+Cafe/@35.466577,-97.5096,17z/data=!3m1!4b1!4m2!3m1!1s0x0:0x8162386058d68e8a?hl=en'})
 venue = Venue.create({ venue: 'Redpin Bowling Lounge', map_link: 'https://www.google.com/maps/place/RedPin+Bowling+Lounge/@35.463632,-97.509842,17z/data=!3m1!4b1!4m2!3m1!1s0x87b2172770ffab75:0x68464e5fb0c7350e?hl=en'})
+
+
+agenda = Agenda.create("2013-07-01", 1, "Project auto scaffolding", "Best Chat Client/Protocol", "Microsoft Ending Technet", "Full Sail Session Black", "Black Mesa Blonde", "Rogue Dead Guy", "Thu, July 18, 2013, 5:30 PM")
+agenda = Agenda.create("2013-08-01", 2, "Yeoman", "Mobile App vs Mobile Web", "Favorite Dev Setup (Hardware/Software)", "Green Flash Hop Head Red", "Left Hand Sawtooth", "Zatec Dark", "Thu, August 22, 2013, 5:30 PM")
+agenda = Agenda.create("2013-09-01", 2, "Postgres 9.3", "Plastic iPhones", "Project Visualization", "Carlsberg Pilsner", "Black Sheep- Monty Python Holy Grail", "North Coast - Prankster", "Thu, September 19, 2013, 5:30 PM")
+
+
+agenda = Agenda.create("2013-10-01", 2, "Ubuntu 13.10 * Windows 8.1", "Proliferation of Cordova", "Application Security", "North Coast - Red Seal Ale", "Ommegang -Three Philosophers", "Santa Fe - Happy Camper IPA", "Thu, October 18, 2013, 5:30 PM")
+agenda = Agenda.create("2013-11-01", 1, "Go-lang", "RabbitMq", "Thunder Plains", "Green Flash Hop Head Red", "Shiner Wild Hare", "Coop Gransport Porter", "Thu, November 21, 2013, 5:30 PM")
+agenda = Agenda.create("2013-12-01", 2, "Cloud IDEs", "Arduino Projects", "D3 Tricks", "Big Sky - Trout Slayer", "Black Mesa - Blonde", "Anchor Steam", "Thu, December 19, 2013, 5:30 PM")
