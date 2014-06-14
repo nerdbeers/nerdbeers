816d26aa6dcf203297b01fa93eeb31567ff2c061
rob sullivan <rob@datachomp.com>
2014-06-14 10:59:18 -0500

    get meeting time in payload

--- app/models/agenda.rb
+++ app/models/agenda.rb
@@ -1,15 +1,14 @@
 class Agenda < ActiveRecord::Base
   belongs_to :venue
-  store_accessor :details, :topic1, :topic2, :topic3, :beer1, :beer2, :beer3
+  store_accessor :details, :topic1, :topic2, :topic3, :beer1, :beer2, :beer3, :meeting_time
   
   def self.get_agenda(meetingdate)
     Agenda.joins(:venue).select('agendas.*, venues.venue as venue_name, venues.map_link as map_link').find_by(:meeting_date => meetingdate)
-#    blah = Agenda.find_by(:meeting_date => meetingdate)
   end
   
-  def self.create(date, location, topic1, topic2, topic3, beer1, beer2, beer3)
+  def self.create(meeting_date, location, topic1, topic2, topic3, beer1, beer2, beer3, meeting_time)
     agenda = Agenda.new
-    agenda.meeting_date = date
+    agenda.meeting_date = meeting_date
     agenda.venue_id = location
     agenda.topic1 = topic1
     agenda.topic2 = topic2
@@ -17,6 +16,7 @@ class Agenda < ActiveRecord::Base
     agenda.beer1 = beer1
     agenda.beer2 = beer2
     agenda.beer3 = beer3
+    agenda.meeting_time = meeting_time
     agenda.save!
   end
   
