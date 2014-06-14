a6fd4c3e8bf3998890db18de04151dd84a259b4f
rob sullivan <rob@datachomp.com>
2014-06-14 11:03:08 -0500

    can't run adminpack extension on heroku

--- db/migrate/20140613195436_enable_hstore_and_other_extensions.rb
+++ db/migrate/20140613195436_enable_hstore_and_other_extensions.rb
@@ -2,6 +2,6 @@ class EnableHstoreAndOtherExtensions < ActiveRecord::Migration
   def change
     execute 'CREATE EXTENSION hstore'
     execute 'CREATE EXTENSION pg_stat_statements'
-    execute 'CREATE EXTENSION adminpack'
+#    execute 'CREATE EXTENSION adminpack'
   end
 end
