1d3258bb7f7a1c891ab8441aa53fa69fe11385ca
rob sullivan <rob@datachomp.com>
2014-07-13 23:43:04 -0500

    dumbfire variant setting on request

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,7 +1,7 @@
 class Metric < ActiveRecord::Base
 
   def self.log_viewport_stuff(variant, user_agent)
-    Metric.create(variant: variant, user_agent: user_agent)
+    Metric.create(variant: variant.to_s, user_agent: user_agent)
   end
 
   def self. get_metrics
