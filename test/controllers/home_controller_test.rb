3d0823adf38aa0ed909eea6206351ac92263da95
rob sullivan <rob@datachomp.com>
2014-07-15 00:22:42 -0500

    very basic template tests for home

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,6 +4,10 @@ class HomeControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    
+    #assert_template :index
+    assert_template "home/index"
+    assert_template layout: "layouts/application"
   end
 
 end
