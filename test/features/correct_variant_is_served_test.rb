86c5b612a5c74936018279f5fbecfe5b41d4808b
Rick Yoesting <rick.yoesting@gmail.com>
2014-07-27 00:01:02 -0500

    move suggestions variants tests to own file

--- test/features/correct_variant_is_served_test.rb
+++ test/features/correct_variant_is_served_test.rb
@@ -29,33 +29,4 @@ class CorrectVariantIsServedTest < Capybara::Rails::TestCase
     assert_not_nil first('p#suggestion-footer')
     assert_equal "tablet", find('input#variant').value
   end
-
-  test "suggestions desktop view is served correctly" do
-
-    visit '/suggestion'
-
-    assert_content page, "NerdBeers"
-    assert_nil first('p#suggestion-footer')
-    assert_equal "desktop", find('input#variant').value
-  end
-
-  test "suggestions mobile view is served correctly" do
-    page.driver.browser.header('User-Agent', 'can has mobile?')
-
-    visit '/suggestion'
-
-    assert_content page, "NerdBeers"
-    assert_nil first('p#suggestion-footer')
-    assert_equal "mobile", find('input#variant').value
-  end
-
-  test "suggestions tablet view is served correctly" do
-    page.driver.browser.header('User-Agent', 'can has ipad/tablet?')
-
-    visit '/suggestion'
-
-    assert_content page, "NerdBeers"
-    assert_nil first('p#suggestion-footer')
-    assert_equal "tablet", find('input#variant').value
-  end
 end
