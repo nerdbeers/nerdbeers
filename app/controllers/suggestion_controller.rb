670f4b6a90ef3a9e157e548cdbfd594bcdcd5cae
Darren Cauthon <darren@cauthon.com>
2014-07-27 18:21:49 -0500

    No need to define these views like this, if they are all the same.
    Conflicts:
    	app/controllers/suggestion_controller.rb

--- app/controllers/suggestion_controller.rb
+++ app/controllers/suggestion_controller.rb
@@ -1,32 +1,24 @@
 class SuggestionController < ApplicationController
-
   before_action :set_variant
-  before_action :log_viewport_stuff, except: :create
 
   def index
     @everything = params[:all].presence
     @suggestions = if @everything then Suggestion.all else Suggestion.last(5) end
     @suggestions = @suggestions.reverse
-    
-    respond_to do |format|
-        format.html          # /app/views/suggestion/index.html.erb
-        format.html.mobile   # /app/views/suggestion/index.html+mobile.erb
-        format.html.tablet   # /app/views/suggestion/index.html+tablet.erb
-    end
+    Metric.log_viewport_stuff(request.variant, request.user_agent)
   end
 
   def new
     if request.post?
       @suggestion = Suggestion.new(suggestion_params)
       redirect_to controller: 'suggestion', action: 'index', status: 303 if @suggestion.save
-    end
+  else
+      Metric.log_viewport_stuff(request.variant, request.user_agent)
   end
-
+  end
+   
   private
-
-  def suggestion_params
-    params.require(:suggestion).permit(:topic, :beer)
-  end
-
+    def suggestion_params
+      params.require(:suggestion).permit(:topic, :beer)
+    end
 end
-
