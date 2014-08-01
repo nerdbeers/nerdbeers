f20e40e06912a8a0549b58065f9837bf5d2a2328
Rick Yoesting <rick.yoesting@gmail.com>
2014-08-01 00:29:21 -0500

    better contoller tests

--- test/controllers/suggestion_controller_test.rb
+++ test/controllers/suggestion_controller_test.rb
@@ -5,30 +5,34 @@ class SuggestionControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     
+    assert_equal "/suggestion", @request.original_fullpath
+    refute_equal "/suggestion/all", @request.original_fullpath
     assert_match /desktop/i, @request.variant.to_s #we have the correct variant
     assert_template "suggestion/index"
     assert_template layout: "layouts/application"
-    assert @response.body =~ /Recent Suggestions/i, "Content should say 'Recent Suggestions'"
   end
 
   test "should get index all" do
-    get(:index, { :all => '', :viewing => :all })
+    get(:index, { :all => 'all', :viewing => :all })
     assert_response :success
 
+    assert_equal "/suggestion/all", @request.original_fullpath
+    refute_equal "/suggestion", @request.original_fullpath
     assert_match /desktop/i, @request.variant.to_s #we have the correct variant
     assert_template "suggestion/index"
     assert_template layout: "layouts/application"
-    assert @response.body =~ /All Suggestions/i, "Content should say 'All Suggestions'"
   end
 
   test "should get new" do
     get :new
     assert_response :success
     
+    assert_equal "/suggestion/new", @request.original_fullpath
+    refute_equal "/suggestion", @request.original_fullpath
+    refute_equal "/suggestion/all", @request.original_fullpath
     assert_match /desktop/i, @request.variant.to_s #we have the correct variant
     assert_template "suggestion/new"
     assert_template layout: "layouts/application"
-    assert @response.body =~ /New Suggestion/i, "Content should say 'New Suggestion'"
   end
 
 end
