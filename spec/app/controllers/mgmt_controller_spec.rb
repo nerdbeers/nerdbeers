bd7dcf4e2c204a30e49c8cd385d5802d53080d2d
Darren Cauthon <darren@cauthon.com>
2014-08-01 12:10:53 -0500

    Test the detail page, with painful unit test.

--- spec/app/controllers/mgmt_controller_spec.rb
+++ spec/app/controllers/mgmt_controller_spec.rb
@@ -2,7 +2,13 @@ require_relative '../../spec_helper'
 
 describe MgmtController do
 
-  let(:controller) { MgmtController.new }
+  let(:params) { {} }
+
+  let(:controller) do
+    c = MgmtController.new
+    c.stubs(:params).returns params
+    c
+  end
 
   it "should be an application controller" do
     controller.is_a?(ApplicationController).must_equal true
@@ -16,6 +22,32 @@ describe MgmtController do
       controller.list
 
       controller.instance_eval { @agendas }.must_be_same_as agendas
+    end
+
+  end
+
+  describe "detail" do
+
+    it "should show the detail for the agenda in question" do
+      agenda_id = Object.new
+      agenda    = Object.new
+
+      # double sigh, the pain of this test is telling me
+      # that this SQL statement belongs elsewhere...
+      # but I'm going to test it as-is for now
+      agenda_with_venue = Object.new
+      agenda_with_venue_and_select = Object.new
+      Agenda.stubs(:joins).with(:venue).returns agenda_with_venue
+      agenda_with_venue.stubs(:select)
+        .with('agendas.*, venues.venue as venue_name, venues.map_link as map_link')
+        .returns agenda_with_venue_and_select
+      agenda_with_venue_and_select.stubs(:find).with(agenda_id).returns agenda
+
+      params[:id] = agenda_id
+      controller.detail
+
+      controller.instance_eval { @agenda }.must_be_same_as agenda
+
     end
 
   end
