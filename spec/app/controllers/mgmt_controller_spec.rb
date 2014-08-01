93b855aebfbe6b3895d1cb70047c191ea6f7574f
Darren Cauthon <darren@cauthon.com>
2014-08-01 12:05:03 -0500

    Test that agendas are passed to the management page list view.

--- spec/app/controllers/mgmt_controller_spec.rb
+++ spec/app/controllers/mgmt_controller_spec.rb
@@ -8,4 +8,16 @@ describe MgmtController do
     controller.is_a?(ApplicationController).must_equal true
   end
 
+  describe "list" do
+
+    it "should return the agendas" do
+      agendas = Object.new
+      Agenda.stubs(:all).returns agendas
+      controller.list
+
+      controller.instance_eval { @agendas }.must_be_same_as agendas
+    end
+
+  end
+
 end
