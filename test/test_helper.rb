1323b4e45b9ee4b04c53440902cec82677b221f0
rob sullivan <rob@datachomp.com>
2014-08-07 17:02:59 -0500

    remove default test helper comments

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,9 +5,6 @@ require "minitest/rails"
 require "minitest/rails/capybara"
 require "minitest/reporters"
 
-# To add Capybara feature tests add `gem "minitest-rails-capybara"`
-# to the test group in the Gemfile and uncomment the following:
-# require "minitest/rails/capybara"
 
 module Minitest
   module Reporters
@@ -53,10 +50,6 @@ Minitest::Reporters.use! [Minitest::Reporters::AwesomeReporter.new(reporter_opti
 class ActiveSupport::TestCase
     ActiveRecord::Migration.check_pending!
 
-    # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
   fixtures :all
 
   def json(body)
