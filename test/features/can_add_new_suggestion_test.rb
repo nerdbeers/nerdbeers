06a79be9ecd901fe42c47fff66c0fc0b0c8e8f4c
Rick Yoesting <rick.yoesting@gmail.com>
2014-08-08 23:53:36 -0500

    factor out some duplicated code

--- test/features/can_add_new_suggestion_test.rb
+++ test/features/can_add_new_suggestion_test.rb
@@ -2,77 +2,67 @@ require "test_helper"
 
 class CanAddNewSuggestionTest < Capybara::Rails::TestCase
   test "new suggestions desktop view is served correctly" do
-
     visit '/suggestions/new'
 
-    assert_content page, "New Suggestion"
-    assert_not_nil first('#suggestion_topic')
-    assert_not_nil first('#suggestion_beer')
-    #assert_equal "desktop", find('input#variant').value
+    common_assert_before_click
   end
 
   test "create new suggestion works as expected with both title and beer provided" do
-
     visit '/suggestions/new'
 
-    assert_content page, "New Suggestion"
-    assert_not_nil first('#suggestion_topic')
-    assert_not_nil first('#suggestion_beer')
     fill_in 'Topic', :with => 'Any Topic'
     fill_in 'Beer', :with => 'Any Beer'
-    click_button 'Save Suggestion'
-    assert current_path == '/suggestions', 'should redirect to /suggestions if successfully created new suggestion'
+
+    common_save_success
   end
 
   test "create new suggestion works as expected with only topic provided" do
-
     visit '/suggestions/new'
 
-    assert_content page, "New Suggestion"
-    assert_not_nil first('#suggestion_topic')
-    assert_not_nil first('#suggestion_beer')
     fill_in 'Topic', :with => 'Any Topic'
-    click_button 'Save Suggestion'
-    assert current_path == '/suggestions', 'should redirect to /suggestions if successfully created new suggestion'
+
+    common_save_success
   end
 
   test "create new suggestion works as expected with only beer provided" do
-
     visit '/suggestions/new'
 
-    assert_content page, "New Suggestion"
-    assert_not_nil first('#suggestion_topic')
-    assert_not_nil first('#suggestion_beer')
     fill_in 'Beer', :with => 'Any Beer'
-    click_button 'Save Suggestion'
-    assert current_path == '/suggestions', 'should redirect to /suggestions if successfully created new suggestion'
+
+    common_save_success
   end
 
   test "create new suggestion with blank fields fails" do
+    visit '/suggestions/new'
+    common_save_fails
+  end
 
+  test "create new suggestion with whitespace fields fails" do
     visit '/suggestions/new'
 
+    fill_in 'Topic', :with => ' '
+    fill_in 'Beer', :with => ' '
+
+    common_save_fails
+  end
+
+  def common_assert_before_click
     assert_content page, "New Suggestion"
     assert_not_nil first('#suggestion_topic')
     assert_not_nil first('#suggestion_beer')
+  end
+
+  def common_save_fails
+    common_assert_before_click
     click_button 'Save Suggestion'
     assert current_path == '/suggestions/new', 'should remain at /suggestions/new if create new suggestion fails'
     assert_not_nil first('div.field_with_errors'), 'should be UI cue that error has occured'
     assert_not_nil first('div.how_to_fix_errors'), 'should be UI message to suggest remedy'
   end
 
-  test "create new suggestion with whitespace fields fails" do
-
-    visit '/suggestions/new'
-
-    assert_content page, "New Suggestion"
-    assert_not_nil first('#suggestion_topic')
-    assert_not_nil first('#suggestion_beer')
-    fill_in 'Topic', :with => ' '
-    fill_in 'Beer', :with => ' '
+  def common_save_success
+    common_assert_before_click
     click_button 'Save Suggestion'
-    assert current_path == '/suggestions/new', 'should remain at /suggestions/new if create new suggestion fails'
-    assert_not_nil first('div.field_with_errors'), 'should be UI cue that error has occured'
-    assert_not_nil first('div.how_to_fix_errors'), 'should be UI message to suggest remedy'
+    assert current_path == '/suggestions', 'should redirect to /suggestions if successfully created new suggestion'
   end
 end
--- test/features/suggestions_correct_variant_is_served_test.rb
+++ test/features/suggestions_correct_variant_is_served_test.rb
@@ -5,21 +5,26 @@ class SuggestionsCorrectVariantIsServedTest < Capybara::Rails::TestCase
 
     visit '/suggestions'
 
-    assert_not_nil first('h3#suggestion-title')
-    assert_equal "Recent Suggestions", find('h3#suggestion-title').text
-    refute_equal "All Suggestions", find('h3#suggestion-title').text
-    assert_nil first('p#suggestion-footer')
-    assert_equal "desktop", find('input#variant').value
+    common_assert
+    common_assert_refute "Recent Suggestions", "All Suggestions"
   end
 
   test "all suggestions desktop view is served correctly" do
 
     visit '/suggestions/all'
 
+    common_assert
+    common_assert_refute  "All Suggestions", "Recent Suggestions"
+  end
+
+  def common_assert
     assert_not_nil first('h3#suggestion-title')
-    assert_equal "All Suggestions", find('h3#suggestion-title').text
-    refute_equal "Recent Suggestions", find('h3#suggestion-title').text
     assert_nil first('p#suggestion-footer')
     assert_equal "desktop", find('input#variant').value
+  end
+
+  def common_assert_refute assert_text, refute_text
+    assert_equal assert_text, find('h3#suggestion-title').text
+    refute_equal refute_text, find('h3#suggestion-title').text
   end
 end
