21551a11c6293f98b2153ee5c37ec8d1800f0e77
Darren Cauthon <darren@cauthon.com>
2014-08-09 07:40:54 -0500

    Style points.

--- app/controllers/api/agenda_controller.rb
+++ app/controllers/api/agenda_controller.rb
@@ -3,18 +3,21 @@ class Api::AgendaController < ApplicationController
   def show
     meeting_date = params[:date].presence || Date.today.at_beginning_of_month
 
-    agenda = Agenda.get_agenda(meeting_date)
-    if agenda.nil?
-    	agenda =  Agenda.get_agenda(nil)
-    end
+    agenda = Agenda.get_agenda(meeting_date) || Agenda.get_agenda(nil)
 
-    pairing1 = { :id => 1, :topic => agenda.topic1, :beer => agenda.beer1 }
-    pairing2 = { :id => 2, :topic => agenda.topic2, :beer => agenda.beer2 }
-    pairing3 = { :id => 3, :topic => agenda.topic3, :beer => agenda.beer3 }
-    pairings = pairing1, pairing2, pairing3
+    pairings = [
+                 { id: 1, topic: agenda.topic1, beer: agenda.beer1 },
+                 { id: 2, topic: agenda.topic2, beer: agenda.beer2 },
+                 { id: 3, topic: agenda.topic3, beer: agenda.beer3 },
+               ]
 
-    @rawr = {:id => agenda.id, :meeting_date => agenda.meeting_date, :pairings => pairings, :venue_name => agenda.venue_name, :map_link => agenda.map_link}
-    render json: @rawr
+    render json: {
+                   id:           agenda.id,
+                   meeting_date: agenda.meeting_date,
+                   pairings:     pairings,
+                   venue_name:   agenda.venue_name,
+                   map_link:     agenda.map_link
+                 }
   end
 
 end
