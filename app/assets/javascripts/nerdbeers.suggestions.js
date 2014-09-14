1e0a8e7e7c376413bcc20c2f1202af139642615b
Rick Yoesting <rick.yoesting@gmail.com>
2014-09-13 21:13:23 -0500

    'suggest' f5 if f5 isn't in the suggestions...

--- app/assets/javascripts/nerdbeers.suggestions.js
+++ app/assets/javascripts/nerdbeers.suggestions.js
@@ -4,7 +4,9 @@ Nerdbeers.Suggestions = {};
 
 Nerdbeers.Suggestions.setupAutocomplete = function() {
     var cache_beer = {},
-    	cache_topic = {};
+    	  cache_topic = {},
+        f5_suggestions = ['Have you considered Coop F5?', 'May I suggest Coop F5?', 'Many nerds like a tasty IPA like COOP F5.'];
+
     $( ".nerdbeers-overflow-beer" ).autocomplete({
       minLength: 2,
       source: function( request, response ) {
@@ -16,7 +18,8 @@ Nerdbeers.Suggestions.setupAutocomplete = function() {
  
         $.getJSON( "/api/agenda/all", request, function( data, status, xhr ) {
           var list = [],
-              re = new RegExp(term, "i");
+              re = new RegExp(term, "i"),
+              reF5 = new RegExp('F5','i');
           $.each(data, function(idx, val){
             $.each(val.pairings, function(i, pair){
               if (re.test(pair.beer)) {
@@ -24,6 +27,15 @@ Nerdbeers.Suggestions.setupAutocomplete = function() {
               }
             });
           });
+
+          var f5 = $.grep(list, function(val) {
+              return reF5.test(val.label);
+          });
+
+          if (f5.length == 0){
+            var item = f5_suggestions[Math.floor(Math.random()*f5_suggestions.length)];
+            list.splice(0, 0, item);
+          }
 
           cache_beer[ term ] = list;
           response( list );
