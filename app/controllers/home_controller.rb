99be2f44e15ebb5200f9f91960aa6725962fbd24
rob sullivan <rob@datachomp.com>
2014-11-15 16:22:28 -0600

    remove response duplication in home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,25 +2,18 @@ class HomeController < ApplicationController
   before_action :set_variant
   before_action :log_viewport_stuff
 
+
+  respond_to do |format|
+      format.html          # /app/views/home/index.html.erb
+      format.html.mobile   # /app/views/home/index.html+mobile.erb
+      format.html.tablet   # /app/views/home/index.html+tablet.erb
+    end
+
   def index
     @agenda = Agenda.get_agenda(params[:date])
-
-    respond_to do |format|
-        format.html          # /app/views/home/index.html.erb
-        format.html.mobile   # /app/views/home/index.html+mobile.erb
-        format.html.tablet   # /app/views/home/index.html+tablet.erb
-    end
   end
 
   def event
     @agenda = Agenda.get_agenda(params[:date])
-
-    respond_to do |format|
-        format.html          # /app/views/home/index.html.erb
-        format.html.mobile   # /app/views/home/index.html+mobile.erb
-        format.html.tablet   # /app/views/home/index.html+tablet.erb
-    end
   end
 end
-
-  
