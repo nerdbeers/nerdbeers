c19d7f972da0f7bad917e6205b432ef8dea0c3a0
rob sullivan <rob@datachomp.com>
2014-11-15 13:56:09 -0600

    silly test on agenda to trigger codeclimate change

--- test/models/agenda_test.rb
+++ test/models/agenda_test.rb
@@ -6,4 +6,20 @@ class AgendaTest < ActiveSupport::TestCase
     blah = Agenda.find(1)
     assert_not_nil blah
 	end
+
+
+
+	test "get this months agenda - no date" do
+		blah = Agenda.get_agenda(nil)
+		latest = Agenda.last
+		assert_not_nil blah
+		assert_equal latest, blah
+	end
+
+	test "get all agendas" do
+		blah = Agenda.get_all
+		assert_not_nil blah
+		#assert_greater blah.count, 0
+	end
+
 end
