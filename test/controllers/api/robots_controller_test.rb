8836cbe5fb5196f2c2e275ee7b4d25351cbdf2a6
rob sullivan <rob@datachomp.com>
2015-04-18 15:19:48 -0500

    robots request and failed auth tests

---
+++ test/controllers/api/robots_controller_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class Api::RobotsControllerTest < ActionController::TestCase
+
+  test "Should fire off vacuum" do
+    token = 'Token token="nerdbeers"'
+    @request.env['HTTP_AUTHORIZATION'] = token
+    # figure out how to test suckerpunch queue
+    post :vacuum
+    assert_response 204
+  end
+  
+  test "should fire off metrics" do
+    token = 'Token token="nerdbeers"'
+    @request.env['HTTP_AUTHORIZATION'] = token
+    post :clearmetrics
+    assert_response 204
+  end
+  
+  test "bad auth token creates death" do
+    token = 'Token token="death"'
+    @request.env['HTTP_AUTHORIZATION'] = token
+    post :vacuum
+    assert_response 401
+  end
+end
