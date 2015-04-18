c27cde038058bec894305c0710a6e129a63ce4ad
rob sullivan <rob@datachomp.com>
2015-04-18 14:42:41 -0500

    add helpful message to metric clear job

--- app/jobs/clear_metrics_job.rb
+++ app/jobs/clear_metrics_job.rb
@@ -3,5 +3,6 @@ class ClearMetricsJob
 
   def perform()
     Metric.delete_all("created_at < (now() - interval '3 months')")
+    Shout.updateteam("We just deleted a bunch of crap in Metrics", 'databot')
   end
 end
