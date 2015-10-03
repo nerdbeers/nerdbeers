0302c409113f8cc693ad6fb22d2db6640da81edf
rob sullivan <rob@datachomp.com>
2015-10-03 14:01:39 -0500

    style fix for venues controller test

--- test/controllers/mgmt/venues_controller_test.rb
+++ test/controllers/mgmt/venues_controller_test.rb
@@ -6,7 +6,7 @@ class Mgmt::VenuesControllerTest < ActionController::TestCase
     @password = ENV["MGMT_PASSWORD"]
     @venue = venues(:one)
   end
-  
+
   test "should stop no credentials" do
     get :index
     assert_response 401
@@ -36,7 +36,7 @@ class Mgmt::VenuesControllerTest < ActionController::TestCase
     params = {venue: { venue: nil, map_link: nil}}
     post "create", params
     assert_response 200
-    
+
     after = Venue.count()
     assert after == before, "Number of venues should remain the same"
   end
@@ -49,7 +49,7 @@ class Mgmt::VenuesControllerTest < ActionController::TestCase
     params = {venue: { venue: "Venue Uno", map_link: "http://nerdbeers.venueuno.com/" }}
     post "create", params
     assert_response 303
-    
+
     after = Venue.count()
     assert after != before, "Number of venues should increase"
     assert after == before + 1, "Number of venues should increase by 1"
@@ -69,7 +69,7 @@ class Mgmt::VenuesControllerTest < ActionController::TestCase
     assert_response 303
 
     v = Venue.last()
-    puts "id: #{v.id}"    
+    puts "id: #{v.id}"
     after = Venue.count()
     assert after != before, "Number of venues should increase"
     assert after == before + 1, "Number of venues should increase by 1"
@@ -92,11 +92,11 @@ class Mgmt::VenuesControllerTest < ActionController::TestCase
     v = Venue.find(@venue.id)
     assert_not_nil v, "updated venue should not be nil"
     assert v.id     == @venue.id, "venue id should match"
-    
+
     assert v.venue == "Venue Uno", "Venue should match"
     assert v.map_link == "http://nerdbeers.venueuno.com/", "map link should match"
-    
-    #ensure we don't match original values
+
+    # ensure we don't match original values
     assert v.venue    != @venue.venue, "Venue should not match"
     assert v.map_link != @venue.map_link, "Map Link should not match"
   end
