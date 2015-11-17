fcbfc31f19a86912283888ab2afde595584aa9ce
rob sullivan <rob@datachomp.com>
2015-11-16 23:16:04 -0600

    add snitch notify to when metrics really clears

--- app/jobs/clear_metrics_job.rb
+++ app/jobs/clear_metrics_job.rb
@@ -3,6 +3,7 @@ class ClearMetricsJob
 
   def perform()
     Metric.delete_all("created_at < (now() - interval '3 months')")
-    Shout.updateteam("We just deleted a bunch of crap in Metrics", 'databot')
+    Shout.updateteam('We just deleted a bunch of crap in Metrics', 'databot')
+    Snitcher.snitch(ENV['METRICS_SNITCH_KEY'], message: 'Adios Metrics')
   end
 end
