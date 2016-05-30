22f9d1f01ff8f7137fcd474c5837d8f8e444fddb
rob sullivan <rob@datachomp.com>
2016-05-29 23:06:23 -0500

    update jobs to use cronplete

--- app/jobs/clear_metrics_job.rb
+++ app/jobs/clear_metrics_job.rb
@@ -3,6 +3,10 @@ class ClearMetricsJob
 
   def perform()
     Metric.delete_all("created_at < (now() - interval '3 months')")
+
+    # check in with cronplete after job runs
+    Net::HTTP.get(URI("http://api.cronplete.com/v1/checking/5b0425d8b640"))
+
     # Shout.updateteam('We just deleted a bunch of crap in Metrics', 'databot')
     Snitcher.snitch(ENV['METRICS_SNITCH_KEY'], message: 'Adios Metrics')
   end
--- app/jobs/vacuum_db_job.rb
+++ app/jobs/vacuum_db_job.rb
@@ -4,6 +4,9 @@ class VacuumDbJob
   def perform()
     # ActiveRecord::Base.connection_pool.with_connection do
       ActiveRecord::Base.connection.execute 'vacuum analyze;'
+
+      # check in with cronplete that the job ran
+      Net::HTTP.get(URI("http://api.cronplete.com/v1/checking/c32e5cfcee77"))
     # end
     # post to slack when it completes
     # Shout.updateteam('vacuum has been completed', 'databot')
