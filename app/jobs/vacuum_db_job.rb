3a21bfa1bdb9e84dc0830f267ede213350971fe7
rob sullivan <rob@datachomp.com>
2016-05-29 23:29:12 -0500

    only checkin in prod

--- app/jobs/vacuum_db_job.rb
+++ app/jobs/vacuum_db_job.rb
@@ -3,10 +3,12 @@ class VacuumDbJob
 
   def perform()
     # ActiveRecord::Base.connection_pool.with_connection do
-      ActiveRecord::Base.connection.execute 'vacuum analyze;'
+    ActiveRecord::Base.connection.execute 'vacuum analyze;'
 
-      # check in with cronplete that the job ran
+    # check in with cronplete after job runs
+    if ENV["RAILS_ENV"] == "production"
       Net::HTTP.get(URI("http://api.cronplete.com/v1/checking/c32e5cfcee77"))
+    end
     # end
     # post to slack when it completes
     # Shout.updateteam('vacuum has been completed', 'databot')
