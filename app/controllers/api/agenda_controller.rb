8ba9d9adba89f265e51dd20891c3efb5a1bfed09
rob sullivan <rob@datachomp.com>
2017-08-15 20:32:31 -0500

    clean up hash syntax in api controllers

--- app/controllers/api/agenda_controller.rb
+++ app/controllers/api/agenda_controller.rb
@@ -4,21 +4,22 @@ class Api::AgendaController < ApplicationController
     meeting_date = params[:date].presence || Date.today.at_beginning_of_month
 
     agenda = Agenda.get_agenda(meeting_date)
+    
     if agenda.nil?
     	agenda =  Agenda.get_agenda(nil)
     end
 
     pairings = buildPairings agenda
 
-    @rawr = {:id => agenda.id, :meeting_date => agenda.meeting_date, :pairings => pairings, :venue_name => agenda.venue_name, :map_link => agenda.map_link}
-	render "api/agenda/show", :formats => [:json], :handlers => [:jbuilder], status: 200
+    @rawr = {id: agenda.id, meeting_date: agenda.meeting_date, pairings: pairings, venue_name: agenda.venue_name, map_link: agenda.map_link}
+	render "api/agenda/show", formats: [:json], handlers: [:jbuilder], status: 200
   end
 
   def all
     unsorted = Array.new
     Agenda.get_all.each do |a|
         pairings = buildPairings a
-        unsorted.push({:id => a.id, :meeting_date => a.meeting_date, :perty_date => a.meeting_date.strftime("%b %d, %Y"), :pairings => pairings, :venue_name => a.venue_name, :map_link => a.map_link})        
+        unsorted.push({id: a.id, meeting_date: a.meeting_date, perty_date: a.meeting_date.strftime("%b %d, %Y"), pairings: pairings, venue_name: a.venue_name, map_link: a.map_link})
     end
 
     # reversing the array is faster than sorting by negative date
@@ -28,9 +29,9 @@ class Api::AgendaController < ApplicationController
   end
 
   def buildPairings agenda
-    pairing1 = { :id => 1, :topic => agenda.topic1, :beer => agenda.beer1 }
-    pairing2 = { :id => 2, :topic => agenda.topic2, :beer => agenda.beer2 }
-    pairing3 = { :id => 3, :topic => agenda.topic3, :beer => agenda.beer3 }
+    pairing1 = { id: 1, topic: agenda.topic1, beer: agenda.beer1 }
+    pairing2 = { id: 2, topic: agenda.topic2, beer: agenda.beer2 }
+    pairing3 = { id: 3, topic: agenda.topic3, beer: agenda.beer3 }
     pairings = pairing1, pairing2, pairing3
   end
 end
--- app/controllers/api/venues_controller.rb
+++ app/controllers/api/venues_controller.rb
@@ -3,13 +3,13 @@ class Api::VenuesController < ApplicationController
   def index
     @venues = Venue.all
  #   render :json => venues, status: 200
-    render "api/venues/index", :formats => [:json], :handlers => [:jbuilder], status: 200
+    render "api/venues/index", formats: [:json], handlers: [:jbuilder], status: 200
   end
 
   def show
     @venue = Venue.find(params[:id])
 #    render :json => venue, status: 200
-    render "api/venues/show", :formats => [:json], :handlers => [:jbuilder], status: 200
+    render "api/venues/show", formats: [:json], handlers: [:jbuilder], status: 200
   end
 
 end
