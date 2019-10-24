63e6bce43d21696738ae719c071d3f87e08deca7
rob sullivan <rob@datachomp.com>
2019-10-24 13:03:52 -0500

    remove cronplete from metrics cleanup

--- app/jobs/clear_metrics_job.rb
+++ app/jobs/clear_metrics_job.rb
@@ -5,9 +5,9 @@ class ClearMetricsJob
     Metric.delete_all("created_at < (now() - interval '3 months')")
 
     # check in with cronplete after job runs
-    if ENV["RAILS_ENV"] == "production"
-      Net::HTTP.get(URI("http://api.cronplete.com/v1/checking/5b0425d8b640"))
-    end
+    #if ENV["RAILS_ENV"] == "production"
+    #  Net::HTTP.get(URI("http://api.cronplete.com/v1/checking/5b0425d8b640"))
+    #end
 
     # Shout.updateteam('We just deleted a bunch of crap in Metrics', 'databot')
     Snitcher.snitch(ENV['METRICS_SNITCH_KEY'], message: 'Adios Metrics')
